4cc747470772bab31cb9db5d05db3fe147fadc70
TechForEverybody <pankajsinghat95@gmail.com>
2022-07-12 11:06:31 +0530

    routing is intiated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,43 @@
 import React from "react";
+import { Routes, Route, NavLink } from "react-router-dom";
+import Temp from "./Temp";
+
 function App() {
     return (
         <>
-            Hello World
+        <NavLink to="/">/</NavLink>
+        <br/>
+        <br/>
+        <NavLink to="/a">/a</NavLink>
+        <br/>
+        <br/>
+        <NavLink to="/b">/b</NavLink>
+        <br/>
+        <br/>
+        <NavLink to="/c">/c</NavLink>
+        <br/>
+        <br/>
+        <NavLink to="/d">/d</NavLink>
+        <br/>
+        <br/>
+        <NavLink to="/e">/e</NavLink>
+        <br/>
+        <br/>
+            <Routes>
+                <Route exact path="/" element={<Temp string="i am home page  "/>}/>
+                <Route exact path="/a" element={<Temp string="i am a page  "/>}/>
+                <Route exact path="/b" element={<Temp string="i am b page  "/>}/>
+                <Route exact path="/c" element={<Temp string="i am c page  "/>}/>
+                <Route exact path="/d" element={<Temp string="i am d page  "/>}/>
+                <Route exact path="/e" element={<Temp string="i am e page  "/>}/>
+            </Routes>
+            <div className="">
+
+            </div>
+            <label htmlFor=""></label>
         </>
     );
 }
 
-export default App;
+
+export default App
---
+++ frontend/src/Temp.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+function Temp(object) {
+    console.log(object);
+    return (
+        <>
+
+            {
+                object.string
+            }
+
+
+        </>
+    )
+}
+
+export default Temp
